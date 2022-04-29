c7b6d4eb74a1ca3b21d935bd53738ba6a26a0343
Metrichor Front-end Team <support@metrichor.com>
2022-04-29 13:25:05 +0100

    checkpoint

--- src/components/wordle-app/wordle-app.tsx
+++ src/components/wordle-app/wordle-app.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, h} from '@stencil/core';
 import WORDLES from '../../assets/wordles.json';
 import { dayOfYear } from '../../utils';
 
